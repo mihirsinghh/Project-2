ca00236a285d8c0821878670d80a6ed589a0d206
Mihir <mihirsingh@berkeley.edu>
2024-11-04 21:22:28 -0800

    changed book desc color

--- frontend/src/pages/BookInfo.js
+++ frontend/src/pages/BookInfo.js
@@ -13,34 +13,38 @@ function BookInfo() {
             sx = {{
                 minHeight: '100vh',
                 backgroundColor: theme.palette.secondary.main,
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
             }}
         >   
             <Box //Box container that holds image and book info
                 sx = {{display: 'flex', flexDirection: 'row', gap: 10}}
-            >
+            >   
+                {/* Image card */}
                 <Card 
-                    sx = {{width: 200}}
+                    sx = {{width: 300}}
                 >
                     <CardMedia
                         component="img"
                         image={book.thumbnail}
                         sx = {{
-                            height: 300,
+                            height: 400,
                             width: '100%',
                             objectFit: 'cover'
                         }}
                     />
                 </Card>
-                
+
                 {/* Box container that holds book info */}        
                 <Box
                     sx = {{display: 'flex', flexDirection: 'column', gap: 1, maxWidth: '60%'}}
                 >
-                    <Typography variant = "h6">{book.title}</Typography>
-                    <Typography variant = "h7">{book.authors}</Typography>
-                    <Typography>{book.publisher}</Typography>
-                    <Typography>{book.publishedDate}</Typography>
-                    <Typography>{book.description}</Typography>  
+                    <Typography variant = "h6" sx = {{color: theme.palette.text.primary}}>{book.title}</Typography>
+                    <Typography variant = "h7" sx = {{color: theme.palette.text.primary}}>{book.authors}</Typography>
+                    <Typography sx = {{color: theme.palette.text.primary}}>{book.publisher}</Typography>
+                    <Typography sx = {{color: theme.palette.text.primary}}>{book.publishedDate}</Typography>
+                    <Typography sx = {{color: theme.palette.text.primary, fontSize: 15}}>{book.description}</Typography>  
                 </Box>
 
 
