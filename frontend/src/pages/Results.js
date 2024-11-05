a2d8509bde7bb0ed3707cee1a527f437601eea4a
Mihir <mihirsingh@berkeley.edu>
2024-11-04 18:10:22 -0800

    removed each book description box in search results

--- frontend/src/pages/Results.js
+++ frontend/src/pages/Results.js
@@ -1,7 +1,5 @@
-// src/pages/Results.js
-
 import React from 'react';
-import {Grid2, Card, CardContent, Typography, CardMedia } from '@mui/material';
+import {Box, Grid2, Card, CardContent, Typography, CardMedia } from '@mui/material';
 import { useLocation } from 'react-router-dom';
 import {useTheme} from '@mui/material/styles';
 
@@ -14,29 +12,26 @@ function Results() {
     //sets up a Grid container with a spacing of "2" between each item, which will be each book-card
     <Grid2 
         container spacing={6} 
-        sx={{backgroundColor: theme.palette.secondary.main}}
+        sx={{
+            backgroundColor: theme.palette.secondary.main,
+            minHeight: '100vh',
+        }}
         justifyContent = "center"
-    >
+    >   
         {/* map each Book item from results array*/}
         {results.map((book) => (
           //each individual Grid item represents a distinct "cell" in the Grid, allowing you to control dimensions and layout of each item
-          <Grid2 item key={book.id}> 
-            <Card sx = {{maxWidth: 300}}>
+          <Grid2 item key={book.id} marginTop = {5}> 
+            <Card sx={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
 
               <CardMedia
                 component="img"
-                height="300"
                 image={book.thumbnail}
                 alt={book.title}
-                sx = {{objectFit: 'cover'}}
+                sx = {{
+                    objectFit: 'contain',
+                }}
               />
-              <CardContent>
-                <Typography variant="h6">{book.title}</Typography>
-                <Typography variant="subtitle1" color="text.secondary">
-                  {book.authors}
-                </Typography>
-              </CardContent>
-
             </Card>
           </Grid2>
         ))}
