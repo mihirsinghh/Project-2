f1cef4ce68644f23ca577561eb927074cefe04d1
Mihir <mihirsingh@berkeley.edu>
2024-11-04 16:55:51 -0800

    kind of aligned cards

--- frontend/src/pages/Results.js
+++ frontend/src/pages/Results.js
@@ -12,23 +12,28 @@ function Results() {
 
   return (
     //sets up a Grid container with a spacing of "2" between each item, which will be each book-card
-    <Grid2 container spacing={8} sx={{backgroundColor: theme.palette.secondary.main}}>
+    <Grid2 
+        container spacing={6} 
+        sx={{backgroundColor: theme.palette.secondary.main}}
+        justifyContent = "center"
+    >
         {/* map each Book item from results array*/}
         {results.map((book) => (
           //each individual Grid item represents a distinct "cell" in the Grid, allowing you to control dimensions and layout of each item
           <Grid2 item key={book.id}> 
-            <Card>
+            <Card sx = {{maxWidth: 300}}>
 
               <CardMedia
                 component="img"
-                height="140"
+                height="300"
                 image={book.thumbnail}
                 alt={book.title}
+                sx = {{objectFit: 'cover'}}
               />
               <CardContent>
                 <Typography variant="h6">{book.title}</Typography>
                 <Typography variant="subtitle1" color="text.secondary">
-                  {book.author}
+                  {book.authors}
                 </Typography>
               </CardContent>
 
